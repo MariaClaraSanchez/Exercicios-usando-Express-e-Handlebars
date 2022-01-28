0827c5f3ea8634c363909f61bb1017004269fb0b
Maria <clarasanchez2398@gmail.com>
2022-01-27 22:37:55 -0300

    Questao

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const path = require('path');
 const bodyParse = require('body-parser');
-const Exercicio_1 = require('./routes/route_exerc_1');
+const Exercicio_1 = require('./routes/route_exerc1');
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,16 +11,12 @@ const app = express();
 app.engine('handlebars', expressHandlebars.engine());
 
 app.set('view engine','handlebars');
-
-//app.set('views','./views');
-app.set('views', path.join(__dirname, './views'));
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(path.join(__dirname,'public')));
 console.log(path.join(__dirname,'public'));
 
 app.use(bodyParse.urlencoded({extended:true}));
-
-
 
 /*Página Home*/
 app.get("/",function(request,response){
@@ -29,19 +25,7 @@ app.get("/",function(request,response){
     })
 });
 
-/*Exercício 1 */
-app.get('/exerc1',async (req,resp) => {
-    resp.render('exerc1',{
-        tituloPagina:  'Exercício Número 1'
-    });
-});
-
-app.post('/exerc1', async (req,resp) => {
-
-    let valor = req.body.valor;
-    console.log(req.body.valor);
-    resp.render('exerc1',valor);
-});
+app.use(Exercicio_1);
 
 app.listen(PORT,function(){
     console.log(`Server is running at port ${PORT}`);
---
+++ src/routes/route_exerc1.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const Router = express.Router();
+const bodyParse = require('body-parser');
+
+/*Exercício 1 */
+Router.get('/exerc1', function (request, response) {
+    response.render('exerc1', {
+        tituloPagina: 'Exercício Número 1'
+    });
+});
+
+Router.post('/exerc1', function (request, response) {
+    const { numero } = request.body;
+    const erro = false;
+
+    console.log(numero);
+
+    if (numero==null) {
+        return response.render('exerc1', {
+            erro: true,
+            mensagemErro: 'Por favor digite algum número pois é um campo obrigatório!!',
+        });
+    }
+
+    if (numero >0)
+        response.render('exerc1', { tipo: 'POSITIVO' }, erro);
+    else if (numero <0)
+        response.render('exerc1', { tipo: 'NEGATIVO' }, erro);
+    else
+        response.render('exerc1', { tipo: 'IGUAL A ZERO' }, erro);
+});
+
+module.exports = Router;
