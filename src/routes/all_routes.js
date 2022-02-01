c89b4bda137d9b5903e15ba09d667a4bd9acd9e0
Maria <clarasanchez2398@gmail.com>
2022-01-31 22:15:01 -0300

    Exercicio 8

--- src/routes/all_routes.js
+++ src/routes/all_routes.js
@@ -9,7 +9,7 @@ const Exercicio_4 = require('./route_exerc4');
 const Exercicio_5 = require('./route_exerc5');
 const Exercicio_6 = require('./route_exerc6');
 const Exercicio_7 = require('./route_exerc7');
-// const Exercicio_8 = require('./route_exerc8');
+const Exercicio_8 = require('./route_exerc8');
 // const Exercicio_9 = require('./route_exerc9');
 // const Exercicio_10 = require('./route_exerc10');
 // const Exercicio_11 = require('./route_exerc11');
@@ -21,10 +21,9 @@ Router.use(Exercicio_4);
 Router.use(Exercicio_5);
 Router.use(Exercicio_6);
 Router.use(Exercicio_7);
-// Router.use(Exercicio_8);
+Router.use(Exercicio_8);
 // Router.use(Exercicio_9);
 // Router.use(Exercicio_10);
 // Router.use(Exercicio_11);
-
 
 module.exports = Router;
---
+++ src/routes/route_exerc8.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const Router = express.Router();
+
+/*Exercício 8*/
+Router.get('/exerc8', function (request, response) {
+    response.render('exerc8', {
+        tituloPagina: 'Exercício Número 8'
+    });
+});
+
+Router.post('/exerc8', function (request, response) {
+    let dados = {
+        salario: request.body.salario_fixo,
+        total_vendas: request.body.valor_venda,
+    };
+    const erro = false;
+
+    if ((!dados.salario)||(!dados.total_vendas)) {
+        return response.render('exerc8', {
+            erro: true,
+            mensagemErro: 'Por favor digite um dos campos!!',
+        });
+    } 
+    
+    var salario_convertido = parseFloat(dados.salario);
+    var valor_venda_convertida = parseFloat(dados.total_vendas);
+    var salario_comissao_1500 = 0
+    var salario_final = 0;
+    var diferenca = 0;
+    
+/*     if(valor_venda_convertida == 0){
+        salario_final = salario_convertido;
+        response.render('exerc8',{salario_final},erro);
+    } */
+    if(valor_venda_convertida > 1500){
+        salario_comissao_1500 = salario_convertido + (1500* 0.03);
+        diferenca = valor_venda_convertida - 1500;
+        salario_final = salario_comissao_1500 + (diferenca * 0.05);
+        //console.log(`salario_1500 ${salario_comissao_1500}`);
+        //console.log(`Diferença ${diferenca}`);
+        //console.log(`Diferença do trem ${(diferenca * 0.05)}`);
+        //console.log(`Salário final ${salario_final}`);
+        response.render('exerc8',{salario_final},erro);
+
+
+    }else{
+        salario_final = salario_convertido + (valor_venda_convertida * 0.03);
+        //console.log(`Salário final ${salario_final}`);
+        response.render('exerc8',{salario_final},erro);
+    }
+
+});
+
+module.exports = Router;
