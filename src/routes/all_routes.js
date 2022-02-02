fddadb5dbcabe6335b86778edd5671a357cf3367
Maria <clarasanchez2398@gmail.com>
2022-02-01 22:14:36 -0300

    Exercicio 2

--- src/routes/all_routes.js
+++ src/routes/all_routes.js
@@ -12,7 +12,7 @@ const Exercicio_7 = require('./route_exerc7');
 const Exercicio_8 = require('./route_exerc8');
 const Exercicio_9 = require('./route_exerc9');
 const Exercicio_10 = require('./route_exerc10');
-// const Exercicio_11 = require('./route_exerc11');
+const Exercicio_11 = require('./route_exerc11');
 
 Router.use(Exercicio_1);
 Router.use(Exercicio_2);
@@ -24,6 +24,6 @@ Router.use(Exercicio_7);
 Router.use(Exercicio_8);
 Router.use(Exercicio_9);
 Router.use(Exercicio_10);
-// Router.use(Exercicio_11);
+Router.use(Exercicio_11);
 
 module.exports = Router;
---
+++ src/routes/route_exerc2.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const Router = express.Router();
+
+/*Exercício 1 */
+Router.get('/exerc2', function (request, response) {
+    response.render('exerc2', {
+        tituloPagina: 'Exercício Número 2'
+    });
+});
+
+Router.post('/exerc2', function (request, response) {
+    const {numero} =  request.body;
+    const erro = false;
+    const tabuada = [];
+
+    if (!numero) {
+        return response.render('exerc2', {
+            tabuada:[],
+            erro: true,
+            mensagemErro: 'Por favor digite algum número pois é um campo obrigatório!!',
+        });
+    }
+
+    for (let i = 0; i <= 10; i++) {
+        tabuada.push(`${numero} X ${i} = ${i*numero}`); 
+        //tabuada[i] = `${dado.numero} X ${i} = ${i*dado.numero}`;
+        //console.log(`${i} * ${numeroConvertido}  = ${tabuada[i]}`);
+    }
+    return response.render('exerc2',{tabuada},erro);
+});
+
+module.exports = Router;
